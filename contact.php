7e5d6b70f98ea157fb7ce5bd727de1a8f14f4b2d
sabrinaparvin12 <sabrinaparvin12@gmail.com>
2021-08-04 13:56:38 +0600

    first commit

---
+++ contact.php
@@ -0,0 +1,17 @@
+<?php 
+$name= $_POST['name'];
+$vistor_email= $_POST['email'];
+$message= $_POST['message'];
+
+$email_from='sabrinaparvin12@gmail.com';
+$email_subject="New Form Submission";
+$email_body="User Name: $name.\n".
+    "User Email: $visitor_email.\n".
+    "User Message: $message.\n";
+$to ="sabrinaparvin12@gmail.com";
+$headers="From:$email_from \r\n";
+$headers="Reply-To:$visitor_email \r\n";
+mail($to,$email_subject,$email_body,$headers);
+header("Location: new.html");
+?>
+    
