7c7e257eb2b90539ca80200af9c8d1c30d53e67c
Sabrina Parvin <88331178+sabrinaparvin12@users.noreply.github.com>
2021-08-07 11:17:02 +0600

    Update contact.php

--- contact.php
+++ contact.php
@@ -12,6 +12,6 @@ $to ="sabrinaparvin12@gmail.com";
 $headers="From:$email_from \r\n";
 $headers="Reply-To:$visitor_email \r\n";
 mail($to,$email_subject,$email_body,$headers);
-header("Location: new.html");
+header("Location: index.html");
 ?>
     
